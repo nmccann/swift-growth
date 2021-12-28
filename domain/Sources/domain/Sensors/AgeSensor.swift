a29bbdd0b08f8a6cfe3473d7bf8391f1d3d01b6b
Noah McCann <nmccann@users.noreply.github.com>
2021-12-27 22:34:01 -0500

    Added an extension to simplify clamping.

--- domain/Sources/domain/Sensors/AgeSensor.swift
+++ domain/Sources/domain/Sensors/AgeSensor.swift
@@ -4,6 +4,6 @@ import Foundation
 /// linearly to normalized sensor range 0.0..1.0
 struct AgeSensor: Sensor {
   func get(for individual: Indiv, simStep: Int, on grid: Grid, with parameters: Params) -> Double {
-    min(max(0.0, Double(individual.age) / Double(parameters.stepsPerGeneration)), 1.0)
+    (Double(individual.age) / Double(parameters.stepsPerGeneration)).clamped(to: 0...1)
   }
 }
--- domain/Sources/domain/Sensors/OscillatorSensor.swift
+++ domain/Sources/domain/Sensors/OscillatorSensor.swift
@@ -3,8 +3,6 @@ import Foundation
 // Maps current location along axis of 0...grid axis dimension-1 to sensor range 0.0..1.0
 struct OscillatorSensor: Sensor {
   func get(for individual: Indiv, simStep: Int, on grid: Grid, with parameters: Params) -> Double {
-    var sensorVal = 0.0
-
     // Maps the oscillator sine wave to sensor range 0.0..1.0;
     // cycles starts at simStep 0 for everybody.
     let phase = Double(simStep % individual.oscPeriod) / Double(individual.oscPeriod) // 0.0..1.0
@@ -13,7 +11,6 @@ struct OscillatorSensor: Sensor {
     factor += 1.0 // convert to 0.0..2.0
     factor /= 2.0 // convert to 0.0..1.0
     // Clip any round-off error
-    sensorVal = factor;
-    return min(1.0, max(0.0, sensorVal))
+    return factor.clamped(to: 0...1)
   }
 }
