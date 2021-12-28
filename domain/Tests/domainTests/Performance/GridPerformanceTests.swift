84e44daf56394cb6e95f855371525ad2a044d67f
Noah McCann <nmccann@users.noreply.github.com>
2021-12-28 07:17:41 -0500

    Added performance test for visitNeighborhood
    Introduced this performance test primarily to determine if an async
    visitNeighborhood would be worthwhile - ultimately, it doesn't seem
    like it would as the async implementation ends up being much slower
    (probably due to large numbers of threads doing small amounts of work).
    There might be a way to do this other than the naive approach I used.

---
+++ domain/Tests/domainTests/Performance/GridPerformanceTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+import Nimble
+@testable import domain
+
+class GridPerformanceTests: XCTestCase {
+  var individuals: [Indiv] = []
+  var sut: Grid!
+
+  override func setUp() {
+    sut = .init(size: (x: 1000, y: 1000))
+    let middle = (x: sut.size.x / 2, y: sut.size.y / 2)
+    individuals = (1...10).map { .stub(index: $0, loc: .init(x: middle.x + $0, y: middle.y + $0)) }
+    individuals.forEach { sut.set(loc: $0.loc, val: $0.index) }
+  }
+
+  func testVisitNeighborhood() {
+    measure {
+      var locations = 0
+      func countLocations(loc: Coord) {
+        locations += 1
+      }
+
+      individuals.forEach {
+        self.sut.visitNeighborhood(loc: $0.loc, radius: 5, f: countLocations(loc:))
+      }
+
+      expect(locations) == 81 * individuals.count
+    }
+  }
+}
